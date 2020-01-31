1e93d382c8482de417d9f002844d589f0ea0c776
krnsree <krnimpuse@gmail.com>
2020-01-31 11:43:21 +0530

    hahahah

---
+++ app/src/main/java/com/example/techutsav/Event_Page.java
@@ -0,0 +1,36 @@
+package com.example.techutsav;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+
+public class Event_Page extends Fragment {
+
+
+    public Event_Page() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event__page, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/techutsav/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.techutsav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new Event_Page()).commit();
+    }
+}
